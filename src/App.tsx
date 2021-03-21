ea7e50ea7801f80568486cc8e63bbb1313896774
EvilCodebot <50321153+EvilCodebot@users.noreply.github.com>
2021-03-21 20:14:40 +1100

    Write secrets component added (#3)
    * ESLint added
    
    * Prettier added
    
    prettier add, eslint-config-prettier added as well
    
    * github action: auto yarn build on push
    
    * Update autobuild.yml
    
    fix syntax error
    
    * Update .eslintrc.js
    
    turn eslint "explicit-module-boundary-types": "off"
    
    * Write secrets component created
    
    * Fixed error

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
+import { useState } from "react";
+
+
 function App() {
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    if (popUpState == false) {
+      setPopUp(!popUpState);
+      console.log("hello");
+    }
+  }
+
+  const [popUpState, setPopUp] = useState(false);
+
+  let popUp;
+
+  if (popUpState == true) {
+    popUp = (
+      <div className="popup">
+        <a href="#">www.secret.com</a>
+        <button onClick={() => setPopUp(!popUpState)}>x</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit} className="center">
+        <textarea className="input"></textarea>
+
+        <input type="submit" value="click here!" className="submitButton" />
+      </form>
+
+      {popUp}
     </div>
   );
 }
