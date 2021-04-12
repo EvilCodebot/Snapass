dad233bc9b390fa48fa232a898a75dad83b657e9
Anlan <50321153+EvilCodebot@users.noreply.github.com>
2021-04-12 21:41:54 +1000

    post & fetch the secret functionality achieved

--- server/index.js
+++ server/index.js
@@ -1,20 +1,73 @@
-const express = require('express')
-const authServiceClient = require('./services/authServiceClient')
+const express = require("express");
+const authServiceClient = require("./services/authServiceClient");
+const axios = require("axios");
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
+let secretID;
 
-app.use('/api/getToken', (req, res) => {
-    authServiceClient
-        .getToken()
-        .then(_ => {
-            console.log(_)
-            res.json(_.data)
+// get token from api first then post secret to api
+app.use("/api/postSecret", (req, res) => {
+  authServiceClient
+    .getToken()
+    .then((_) => {
+      console.log(_.data.access_token);
+      console.log("---------------------------");
+
+      axios
+        .post(
+          "https://ikeb1e8o0e.execute-api.ap-southeast-2.amazonaws.com/api/secret",
+          { secret: "This is a test baby" },
+          {
+            headers: { Authorization: `Bearer ${_.data.access_token}` },
+          }
+        )
+        .then((_) => {
+          secretID = _.data.id;
+
+          res.json("The secret id is: " + secretID);
+          console.log("The secret id is: " + secretID);
+          console.log("---------------------------");
         })
         .catch((err) => {
-            console.log(err)
-           
-        })
-})
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.listen(port, () => console.log(`Sample expressjs is listening at port ${port}`))
+// get token from api first then get secret from api
+app.use("/api/getSecret", (req, res) => {
+  authServiceClient
+    .getToken()
+    .then((_) => {
+      console.log(_.data.access_token);
+      console.log("---------------------------");
+
+      axios
+        .get(
+          "https://ikeb1e8o0e.execute-api.ap-southeast-2.amazonaws.com/api/secret/" +
+            secretID,
+          {
+            headers: { Authorization: `Bearer ${_.data.access_token}` },
+          }
+        )
+        .then((_) => {
+          res.json("The message of the secret is: " + _.data.body);
+          console.log("The message of the secret is: " + _.data.body);
+          console.log("---------------------------");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(port, () =>
+  console.log(`Sample expressjs is listening at port ${port}`)
+);
