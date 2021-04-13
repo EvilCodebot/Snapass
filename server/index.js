a2e109519c9ea5f218535ddebe979dd8f9c85576
Anlan <50321153+EvilCodebot@users.noreply.github.com>
2021-04-13 11:49:27 +1000

    get&fetch functionality refactored
    refactored according to single responsibility principle.

--- server/index.js
+++ server/index.js
@@ -6,24 +6,17 @@ const app = express();
 const port = 4000;
 let secretID;
 
-// get token from api first then post secret to api
 app.use("/api/postSecret", (req, res) => {
   authServiceClient
     .getToken()
-    .then((_) => {
-      console.log(_.data.access_token);
+    .then((value) => {
+      console.log(value.data.access_token);
       console.log("---------------------------");
 
-      axios
-        .post(
-          "https://ikeb1e8o0e.execute-api.ap-southeast-2.amazonaws.com/api/secret",
-          { secret: "This is a test baby" },
-          {
-            headers: { Authorization: `Bearer ${_.data.access_token}` },
-          }
-        )
-        .then((_) => {
-          secretID = _.data.id;
+      authServiceClient
+        .postSecret(value.data.access_token)
+        .then((value) => {
+          secretID = value.data.id;
 
           res.json("The secret id is: " + secretID);
           console.log("The secret id is: " + secretID);
@@ -42,21 +35,15 @@ app.use("/api/postSecret", (req, res) => {
 app.use("/api/getSecret", (req, res) => {
   authServiceClient
     .getToken()
-    .then((_) => {
-      console.log(_.data.access_token);
+    .then((value) => {
+      console.log(value.data.access_token);
       console.log("---------------------------");
 
-      axios
-        .get(
-          "https://ikeb1e8o0e.execute-api.ap-southeast-2.amazonaws.com/api/secret/" +
-            secretID,
-          {
-            headers: { Authorization: `Bearer ${_.data.access_token}` },
-          }
-        )
-        .then((_) => {
-          res.json("The message of the secret is: " + _.data.body);
-          console.log("The message of the secret is: " + _.data.body);
+      authServiceClient
+        .getSecret(value.data.access_token, secretID)
+        .then((value) => {
+          res.json("The message of the secret is: " + value.data.body);
+          console.log("The message of the secret is: " + value.data.body);
           console.log("---------------------------");
         })
         .catch((err) => {
--- server/services/authServiceClient.js
+++ server/services/authServiceClient.js
@@ -1,9 +1,25 @@
-const axios = require('axios')
-const config = require('../config.json')
+const axios = require("axios");
+const config = require("../config.json");
 
 module.exports = {
-    getToken: () => { 
-        const {baseUrl, scope, apiKeyId, secretAccessKey} = config.authService;
-        return axios.post(baseUrl, { scope, apiKeyId, secretAccessKey })
-    }
-}
+  getToken: () => {
+    const { baseUrl, scope, apiKeyId, secretAccessKey } = config.authService;
+    return axios.post(baseUrl, { scope, apiKeyId, secretAccessKey });
+  },
+
+  postSecret: (token) => {
+    return axios.post(
+      config.apiBaseUrlSecret,
+      { secret: "This is a test baby" },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+  },
+
+  getSecret: (token, SecretID) => {
+    return axios.get(config.apiBaseUrlSecret + SecretID, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  },
+};
